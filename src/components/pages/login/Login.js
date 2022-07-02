9ed7f9ed10fbf9c0d9b1cb7dc7f4d24c7fdf117b
Jefferson Oliveira <jeffebido@hotmail.com>
2022-07-02 17:22:09 -0300

    Login e SignUp

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,9 +1,69 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import {useAuth} from "../../../providers/Auth"; 
+
 export default function Login() {
+
+    const navigate = useNavigate();
+
+    const [formEmail, setFormEmail] = useState("");
+    const [formPassword, setFormPassword] = useState("");
 
     return (
 
         <>  
+            <Container>
+                <Logo>MyWallet</Logo>
 
+                <form>
+                    <input type="email" placeholder="E-mail" value={formEmail} onChange={e => setFormEmail(e.target.value)} required ></input>
+                    <input type="password" placeholder="Senha" value={formPassword} onChange={e => setFormPassword(e.target.value)} required ></input>
+                    
+                    <button type="submit" className="btn">ENTRAR</button>
+                </form>
+
+                <Footer>
+                    <Link to={`/sign-up`} >
+                        Não tem uma conta? Cadastre-se!
+                    </Link>
+                </Footer>
+            </Container>
         </>
     );
 }
+
+const Container = styled.div`
+	width: 100%;
+    height: 100vh;
+	background: #8C11BE;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding-left: 40px;
+    padding-right: 40px;
+    input, .btn{
+        margin-bottom: 15px;
+    }
+`;
+const Logo = styled.div`
+	font-family: 'Paytone One', sans-serif;
+    color: #fff;
+    font-size: 40px;
+    margin-bottom: 50px;
+`;
+const Footer = styled.div`
+    margin-top: 35px;
+    a{
+        font-style: normal;
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 18px;
+        text-align: center;
+        text-decoration: none;
+        color: #fff;
+        cursor: pointer;
+    }
+`;
--- src/components/pages/sign-up/SignUp.js
+++ src/components/pages/sign-up/SignUp.js
@@ -1,9 +1,72 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import {useAuth} from "../../../providers/Auth"; 
+
 export default function SignUp() {
+
+
+    const [formName, setFormName] = useState("");
+    const [formEmail, setFormEmail] = useState("");
+    const [formPassword, setFormPassword] = useState("");
+    const [formConfirmPassword, setFormConfirmPassword] = useState("");
 
     return (
 
         <>  
+            <Container>
+                <Logo>MyWallet</Logo>
 
+                <form>
+                    <input type="text" placeholder="Nome" value={formName} onChange={e => setFormName(e.target.value)} required ></input>
+                    <input type="email" placeholder="E-mail" value={formEmail} onChange={e => setFormEmail(e.target.value)} required ></input>
+                    <input type="password" placeholder="Senha" value={formPassword} onChange={e => setFormPassword(e.target.value)} required ></input>
+                    <input type="password" placeholder="Confirme a senha" value={formConfirmPassword} onChange={e => setFormConfirmPassword(e.target.value)} required ></input>
+
+                    <button type="submit" className="btn">ENTRAR</button>
+                </form>
+
+                <Footer>
+                    <Link to={`/`} >
+                        Já tem uma conta? Entre agora!
+                    </Link>
+                </Footer>
+            </Container>
         </>
     );
 }
+
+const Container = styled.div`
+	width: 100%;
+    height: 100vh;
+	background: #8C11BE;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding-left: 40px;
+    padding-right: 40px;
+    input, .btn{
+        margin-bottom: 15px;
+    }
+`;
+const Logo = styled.div`
+	font-family: 'Paytone One', sans-serif;
+    color: #fff;
+    font-size: 40px;
+    margin-bottom: 50px;
+`;
+const Footer = styled.div`
+    margin-top: 35px;
+    a{
+        font-style: normal;
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 18px;
+        text-align: center;
+        text-decoration: none;
+        color: #fff;
+        cursor: pointer;
+    }
+`;
